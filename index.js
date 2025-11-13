3d56122196a18a6f2d8f8b472a7c15d0d378aae6
unknown <shaching.contact@gmail.com>
2025-11-13 16:54:38 +0600

    added successfully

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ const client = new MongoClient(uri, {
   },
 });
 
+console.log(process.env.DB_USERNAME);
+console.log(process.env.DB_PASSWORD);
+
 const db = client.db("heroHomeDB");
 const serviceCollections = db.collection("services");
 const bookedCollections = db.collection("bookings");
@@ -30,7 +33,7 @@ const reviewCollections = db.collection("reviews");
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     // ---------services related apis here
     app.post("/services", async (req, res) => {
       const newService = req.body;
@@ -217,7 +220,7 @@ async function run() {
       res.send(reviews);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
